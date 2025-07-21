2176cf39ed6b4313ae0e5937863aeca6e643c00b
Ajax11 <alejandro.delgado@ucsp.edu.pe>
2025-07-21 03:35:38 -0500

    Adding the models

--- backend/reader/admin.py
+++ backend/reader/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Author, Chapter, Manga
+
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Chapter)
+admin.site.register(Manga)
--- backend/reader/models.py
+++ backend/reader/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200)
+    role_options = [
+        ('main','Lead Author'),
+        ('supp','Assistant'),
+    ]
+    role = models.CharField(max_length=10, choices=role_options, default='main')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Manga(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
+
+
+class Chapter(models.Model):
+    chapter_number = models.PositiveSmallIntegerField()
+    directory_path = models.CharField(max_length=300)
+    upload_date = models.DateTimeField(auto_now=True)
+    manga = models.ForeignKey(Manga, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Chapter number {self.chapter_number}"
+
